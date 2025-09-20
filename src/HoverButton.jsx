a4b12bbe1bcf934eefb55c82a601b3fdd954be20
HyunJunggggg <hyunjung9219@gmail.com>
2025-09-19 19:26:56 -0700

    initial commit

---
+++ src/HoverButton.jsx
@@ -0,0 +1,11 @@
+function HoverButton() {
+    const HoverButtonLabel = "New";
+    return (
+      <div className="book">
+        <button className="hover-button">{HoverButtonLabel}</button>
+      </div>
+    );
+  }
+  
+  export default HoverButton;
+  
